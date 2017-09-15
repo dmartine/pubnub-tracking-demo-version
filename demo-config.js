51b41aec11cf4bc588aabebf659c855fbde02e67
Nicholas Furness <nfurness@nixta.com>
2017-09-15 18:06:02 -0400

    demo config

---
+++ demo-config.js
@@ -0,0 +1,16 @@
+define([], function(){
+    return {
+        serviceURL: "https://services.arcgis.com/OfH668nDRN7tbJh0/arcgis/rest/services/TCSF_Delivery_Tracking_Demo/FeatureServer",
+        simServiceURL: "https://services.arcgis.com/OfH668nDRN7tbJh0/arcgis/rest/services/TCSF_Delivery_Driver_Sim/FeatureServer",
+        simRouteIds: [
+            'a91da7bc-b57a-4fca-8070-cecf53a0f3a9',
+            '2a25b4c1-96b9-4c3c-8b52-b9540db90aee',
+            'ef2bce07-233e-4d83-98f8-6bb365858fa9',
+            '84900e49-3e20-4234-8e83-2cdf3e583e39',
+            'c633e9b3-1688-4fad-9183-247a86bfe818'
+        ],
+        driverId: '220c9781-deb7-40f8-8452-2b6e238d9c33',
+        center: [-73.97, 40.75],
+        zoom: 12
+    };
+});
